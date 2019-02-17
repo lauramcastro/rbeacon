6bac2bda2dbf04cf62c58f02d9b7de8527d1b32f
Laura M. Castro <lcastro@udc.es>
2019-02-18 00:07:34 +0100

    ADD sample PBT property and stateful module

--- test/prop_rbeacon.erl
+++ test/prop_rbeacon.erl
@@ -1,5 +1,27 @@
 -module(prop_rbeacon).
 -include_lib("proper/include/proper.hrl").
 
-prop_rbeacon() ->
-    ?FORALL(_A, any(), true). % always passes
+prop_can_send_and_receive_any_string() ->
+    ?FORALL(String, string(),
+            begin
+                {ok, Service} = rbeacon:new(9999),
+                ok = rbeacon:set_interval(Service, 100),
+                ok = rbeacon:publish(Service, unicode:characters_to_binary(String)),
+                
+                {ok, Client} = rbeacon:new(9999),
+                ok = rbeacon:subscribe(Client, ""),
+                
+                {ok, Msg, _Addr} = rbeacon:recv(Client),
+                
+                ok = rbeacon:close(Service),
+                ok = rbeacon:close(Client),
+                
+                % collect(length(String),
+                % measure("Lonxitude do string",
+                %        length(String),
+                % the assert must be the last thing the property does
+                equals(unicode:characters_to_binary(String), Msg)
+                %        )
+
+            end).
+
---
+++ test/prop_rbeacon_stateful.erl
@@ -0,0 +1,58 @@
+-module(prop_rbeacon_stateful).
+-include_lib("proper/include/proper.hrl").
+
+-export([command/1, initial_state/0, next_state/3,
+         precondition/2, postcondition/3]).
+-export([close/1]).
+
+-record(test_state,{rbeacon=null}).
+
+%% @doc Default property
+prop_prop_rbeacon_stateful() ->
+    ?FORALL(Cmds, commands(?MODULE),
+            begin
+                {H, S, Res} = run_commands(?MODULE, Cmds),
+                ?WHENFAIL(io:format("History: ~p\nState: ~p\nResult: ~p\n",
+                                    [H, S, Res]),
+                          aggregate(command_names(Cmds), Res =:= ok))
+            end).
+
+%% @doc Returns the state in which each test case starts.
+initial_state() ->
+    #test_state{}.
+
+%% @doc Command generator, S is the current state
+command(S) ->
+    oneof([{call, rbeacon, new, [user_udp_port()]},
+           {call, ?MODULE, close, [S#test_state.rbeacon]}
+          ]).
+
+% UDP ports 49152 through 65535
+user_udp_port() ->
+    integer(49152, 65535).
+
+%% @doc Next state transformation, S is the current state. Returns next state.
+next_state(S, V, {call, rbeacon, new, _Port}) ->
+    S#test_state{rbeacon = V};
+next_state(S, _V, {call, ?MODULE, close, _Beacon}) ->
+    S#test_state{rbeacon = null}.
+
+
+%% @doc Precondition, checked before command is added to the command sequence.
+precondition(S, {call, rbeacon, new, _Port}) ->
+    S#test_state.rbeacon == null;
+precondition(S, {call, ?MODULE, close, _Beacon}) ->
+    S#test_state.rbeacon =/= null;
+precondition(_S, _Call) ->
+    false.
+
+
+%% @doc Postcondition, checked after command has been evaluated
+%%      Note: S is the state before next_state(S, _, Call)
+postcondition(_S, {call, rbeacon, new, _Port}, {ok, Beacon}) ->
+    is_pid(Beacon);
+postcondition(_S, _Call, _Res) ->
+    true.
+
+close({ok, Beacon}) ->
+    rbeacon:close(Beacon).
