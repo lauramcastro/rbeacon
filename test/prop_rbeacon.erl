5083d433150290844878bc2aa21c3379e052b19e
Laura M. Castro <lcastro@udc.es>
2019-02-18 18:33:13 +0100

    ADD new command to stateful module

--- test/prop_rbeacon.erl
+++ test/prop_rbeacon.erl
@@ -2,7 +2,8 @@
 -include_lib("proper/include/proper.hrl").
 
 prop_can_send_and_receive_any_string() ->
-    ?FORALL(String, string(),
+    numtests(10,
+       ?FORALL(String, string(),
             begin
                 {ok, Service} = rbeacon:new(9999),
                 ok = rbeacon:set_interval(Service, 100),
@@ -17,11 +18,13 @@ prop_can_send_and_receive_any_string() ->
                 ok = rbeacon:close(Client),
                 
                 % collect(length(String),
-                % measure("Lonxitude do string",
-                %        length(String),
+                measure("Lonxitude do string",
+                        length(String),
                 % the assert must be the last thing the property does
                 equals(unicode:characters_to_binary(String), Msg)
-                %        )
+                        )
 
-            end).
+            end
+             )
+        ).
 
--- test/prop_rbeacon_stateful.erl
+++ test/prop_rbeacon_stateful.erl
@@ -3,9 +3,10 @@
 
 -export([command/1, initial_state/0, next_state/3,
          precondition/2, postcondition/3]).
--export([close/1]).
+-export([close/1, publish/2]).
 
--record(test_state,{rbeacon=null}).
+-record(test_state,{rbeacon=null,
+                    message=null}).
 
 %% @doc Default property
 prop_prop_rbeacon_stateful() ->
@@ -24,7 +25,8 @@ initial_state() ->
 %% @doc Command generator, S is the current state
 command(S) ->
     oneof([{call, rbeacon, new, [user_udp_port()]},
-           {call, ?MODULE, close, [S#test_state.rbeacon]}
+           {call, ?MODULE, close, [S#test_state.rbeacon]},
+           {call, ?MODULE, publish, [S#test_state.rbeacon, binary()]}
           ]).
 
 % UDP ports 49152 through 65535
@@ -35,13 +37,17 @@ user_udp_port() ->
 next_state(S, V, {call, rbeacon, new, _Port}) ->
     S#test_state{rbeacon = V};
 next_state(S, _V, {call, ?MODULE, close, _Beacon}) ->
-    S#test_state{rbeacon = null}.
+    S#test_state{rbeacon = null};
+next_state(S, _V, {call, ?MODULE, publish, [_Beacon, Binary]}) ->
+    S#test_state{message = Binary}.
 
 
 %% @doc Precondition, checked before command is added to the command sequence.
 precondition(S, {call, rbeacon, new, _Port}) ->
     S#test_state.rbeacon == null;
 precondition(S, {call, ?MODULE, close, _Beacon}) ->
+    S#test_state.rbeacon =/= null;
+precondition(S, {call, ?MODULE, publish, [_Beacon, _Binary]}) ->
     S#test_state.rbeacon =/= null;
 precondition(_S, _Call) ->
     false.
@@ -51,8 +57,14 @@ precondition(_S, _Call) ->
 %%      Note: S is the state before next_state(S, _, Call)
 postcondition(_S, {call, rbeacon, new, _Port}, {ok, Beacon}) ->
     is_pid(Beacon);
+postcondition(_S, {call, ?MODULE, publish, [_Beacon, _Binary]}, ok) ->
+    true;
 postcondition(_S, _Call, _Res) ->
     true.
 
+%% Wrappers para el modelo
 close({ok, Beacon}) ->
     rbeacon:close(Beacon).
+
+publish({ok, Beacon}, Binary) ->
+    rbeacon:publish(Beacon, Binary).
